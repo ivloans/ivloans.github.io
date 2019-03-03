bc605eed6cf5a01797bf142f6f909287191248f4
Isaac Vidrine <idvidrine@gmail.com>
2019-03-03 13:21:08 -0500

    made mobile and responsive changes

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
             selectedModel: '',
             selectedTrim: '',
             mileage: null,
+            desiredAmount: '',
             modelAccepted: false,
             agreesToMessaging: false,
             firstName: '',
